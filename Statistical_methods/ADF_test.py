73d17d05a26d0597f34041df5e75382057429253
Hans Skinstad <43535291+heskinstad@users.noreply.github.com>
2024-04-23 14:36:34 +0200

    Added and cleaned up MET data

--- Statistical_methods/ADF_test.py
+++ Statistical_methods/ADF_test.py
@@ -2,11 +2,16 @@
 # From: https://www.machinelearningplus.com/time-series/augmented-dickey-fuller-test/
 import os
 
+from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.tsa.stattools import adfuller
 
 import Parameters
 from LSTM import Create_sample_target
 from LSTM.Create_sample_target import create_sample_target_ARIMA
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.use('TkAgg')
 
 df, _ = Create_sample_target.create_sample_gap_prediction(os.getcwd() + r"\data\Munkholmen\all_hourly_fixed.csv")
 
@@ -21,3 +26,6 @@ for key, value in result[4].items():
     print('Critial Values:')
     print(f'   {key}, {value}')
 
+
+plot_acf(df[:50])
+plt.show()
--- util/Get_data_from_MET.py
+++ util/Get_data_from_MET.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python
 # Read data from an opendap server
+import datetime
+
 import netCDF4
+import numpy as np
 
 # Data from MET, at Munkholmen position in correct data period
 
 # specify an url, the JARKUS dataset in this case
 #url = 'https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be?lat[451:1:451][956:1:956],lon[451:1:451][956:1:956],temperature[45325:1:61704][1:1:1][451:1:451][956:1:956]'
 #url = 'https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be?temperature[42229:1:58416][1:1:1][451:1:451][956:1:956],time[42229:1:42229]'
-url = 'https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be?temperature[42229:1:42329][1:1:1][451:1:451][956:1:956]'
+url = 'https://thredds.met.no/thredds/dodsC/sea/norkyst800m/1h/aggregate_be?temperature[50000:1:58416][1:1:1][451:1:451][956:1:956],time[50000:1:58416]'
 # for local windows files, note that '\t' defaults to the tab character in python, so use prefix r to indicate that it is a raw string.
 #url = r'f:\opendap\rijkswaterstaat\jarkus\profiles\transect.nc'
 # create a dataset object
 dataset = netCDF4.Dataset(url)
 
 # lookup a variable
-temp = dataset.variables['temperature']
-#variable2 = dataset.variables['lat']
-#variable3 = dataset.variables['lon']
-# print the first 10 values
-print(temp[:])
-#print(dataset.variables['time'][0])
-#print(variable2[0])
-#print(variable3[0])
+temp = dataset.variables['temperature'][:]
+time = dataset.variables['time'][:]
 
 import pandas as pd
 
+time_ts = pd.Series(np.squeeze(time))
+id = np.arange(7773, 7773+len(time_ts), dtype=int)
+
+for i in range(len(time_ts)):
+    time_ts[i] = datetime.datetime.fromtimestamp(time_ts[i])
+
+temp_ts = pd.Series(np.squeeze(temp), index=[id, time_ts])
+
+temp_ts.to_csv("MET_temp.csv", index=True, header=False)
