141c0b97e0fde1d557fc5fbf675a645f034c1637
Hans Skinstad <heskinstad@gmail.com>
2024-05-07 22:56:43 +0200

    Added dates instead of hours to LSTM prediction

--- LSTM/Plot_data.py
+++ LSTM/Plot_data.py
@@ -2,8 +2,12 @@ import math
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.dates as mdates
 
+import numpy as np
+import pandas as pd
+
+import LSTM.Process_csv
 import Parameters
 
 mpl.use('TkAgg')
@@ -20,21 +24,27 @@ def plot_data(original_data, predicted_data, start=Parameters.series_prediction_
     original_data = original_data[start - Parameters.lookback - 100:start + Parameters.length_of_prediction + Parameters.lookforward + 100]
     predicted_data = predicted_data[start - Parameters.lookback - 100:start + Parameters.length_of_prediction + Parameters.lookforward + 100]
 
+    dates = pd.to_datetime(LSTM.Process_csv.process_csv_column(Parameters.path_test_data, 1, has_header=True, datetimes=True)[start - Parameters.lookback - 100:start + Parameters.length_of_prediction + Parameters.lookforward + 100])
 
-    plt.figure(figsize=(10,6))
-    plt.plot(original_data, c='b', label="True data")
-    plt.plot(predicted_data, c='r', label="Prediction")
-    plt.plot(missing_data, '--', c='b', label="True data (missing)")
+    fig, ax = plt.subplots()
+    ax.plot(dates, original_data, c='b', label="True data")
+    ax.plot(dates, predicted_data, c='r', label="Prediction")
+    ax.plot(dates, missing_data, '--', c='b', label="True data (missing)")
 
     '''if Parameters.multiple_variables:
         sample2_array = np.empty(100+Parameters.lookback+Parameters.lookforward+Parameters.length_of_prediction)
         sample2_array[:] = math.nan
         sample2_array[100+Parameters.lookback:100+Parameters.lookback+Parameters.length_of_prediction] = sample2[10:-10]
-        plt.plot(sample2_array, '--', c='y', label="True data (2nd dataset)")'''
+        ax.plot(dates, sample2_array, '--', c='y', label="True data (2nd dataset)")'''
 
-    plt.axvline(100, ls='--', color='darkgray')
-    plt.axvline(100 + Parameters.lookback + Parameters.length_of_prediction + Parameters.lookforward, ls='--', color='darkgray')
+    ax.axvline(dates[100], ls='--', color='darkgray')
+    ax.axvline(dates[100 + Parameters.lookback + Parameters.length_of_prediction + Parameters.lookforward], ls='--', color='darkgray')
     plt.xlabel("Time (h)")
     plt.ylabel("Temperature")
     plt.legend()
+    from matplotlib.ticker import MaxNLocator
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(MaxNLocator(10))  # Limit the number of x-axis ticks
+    plt.gcf().autofmt_xdate()  # Auto rotates dates for better readability
+
     plt.show()
